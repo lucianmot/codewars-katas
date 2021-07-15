dfd5010716db23a1afbb664da8e21da116e00bf5
lucian <37814580+lucianmot@users.noreply.github.com>
2021-07-15 08:52:59 +0100

    Create Java - Remove String Spaces.java

---
+++ Java - Remove String Spaces.java
@@ -0,0 +1,8 @@
+/* Java - Remove String Spaces
+https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/java */
+
+class Kata {
+    static String noSpace(final String x) {
+        return x.replaceAll("\\s+","");
+    }
+}
