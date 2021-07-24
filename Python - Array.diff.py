aa4c52dfc329e937f6442066bd9bf2403d575407
lucian <37814580+lucianmot@users.noreply.github.com>
2021-07-24 09:25:29 +0100

    Update and rename Array.diff to Python - Array.diff.py

--- Python - Array.diff.py
+++ Python - Array.diff.py
@@ -1,5 +1,5 @@
-#Array.diff
-#https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
+# Python - Array.diff
+# https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
 
 def array_diff(a, b):
     result = a
