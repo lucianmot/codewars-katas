0c5d74f6c891d2a05d5b6e8fb7faa17c440d3c67
lucian <37814580+lucianmot@users.noreply.github.com>
2021-07-26 14:32:00 +0100

    Update Java - Convert string to camel case.java

--- Java - Convert string to camel case.java
+++ Java - Convert string to camel case.java
@@ -3,10 +3,17 @@ https://www.codewars.com/kata/517abf86da9663f1d2000003/train/java */
 
 import java.lang.StringBuilder;
 class Solution{
-
-  static String toCamelCase(String s){
-    s.replaceAll("[-]", "_");
-    return "";
+  static String toCamelCase(String s){    
+    s = s.replaceAll("-", "_");
+    String [] a = s.split("_");
+    
+    s = "";
+    s += a[0];
+    
+    for (int i=1; i<a.length; i++)      
+      s += a[i].substring(0, 1).toUpperCase() + a[i].substring(1).toLowerCase();
+    
+    return s;
   }
 }
 
