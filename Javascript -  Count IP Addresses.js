0524437fecea68bdd48ef87fdab2f39c71f86728
lucian <37814580+lucianmot@users.noreply.github.com>
2021-08-31 09:36:14 +0100

    Create Javascript -  Count IP Addresses.js

---
+++ Javascript -  Count IP Addresses.js
@@ -0,0 +1,13 @@
+// Javascript -  Count IP Addresses
+// https://www.codewars.com/kata/526989a41034285187000de4/train/javascript
+
+function ipsBetween(start, end){
+  start = start.split('.');
+  end = end.split('.');
+  
+  let result = 0;
+  for (let i=0; i<4; i++)
+      result += (parseInt(end[i]) - parseInt(start[i]))*(Math.pow(256,3-i));    
+  
+  return result;  
+}
