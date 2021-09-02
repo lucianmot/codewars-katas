08a8410e72bd6dfff7553f65672fb5b698894781
lucian <37814580+lucianmot@users.noreply.github.com>
2021-09-02 15:37:14 +0100

    Create Python - Valid Phone Number.py

---
+++ Python - Valid Phone Number.py
@@ -0,0 +1,6 @@
+# Python - Valid Phone Number
+# https://www.codewars.com/kata/525f47c79f2f25a4db000025/train/python
+
+import re
+def valid_phone_number(phone_number):
+    return bool(re.match("^[(][0-9]{3}[)] [0-9]{3}-[0-9]{4}$", phone_number))
