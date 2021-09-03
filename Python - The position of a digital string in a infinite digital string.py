16f94f5b2dc83b8d7d29efca0279a4d76f9c3e5a
lucian <37814580+lucianmot@users.noreply.github.com>
2021-09-03 09:34:09 +0100

    Update Python - The position of a digital string in a infinite digital string.py

--- Python - The position of a digital string in a infinite digital string.py
+++ Python - The position of a digital string in a infinite digital string.py
@@ -1,6 +1,7 @@
 # Python - The position of a digital string in a infinite digital string
 # https://www.codewars.com/kata/582c1092306063791c000c00/train/python
 
+# attempt 1
 def find_position(string):
     results = ''
     count = 1
@@ -10,3 +11,31 @@ def find_position(string):
     return results.find(string)
 
 # while loop daca nu resultat positiv cut last 10-15 chars and rebuild string loop search again
+
+# attempt 2
+def find_position(string):
+    results = ''
+    count = 1
+    countLimit = 1
+    indexCount = 15
+    capLimit = 0
+
+    while results.find(string) == -1 and capLimit < 5000000:
+        indexCount = indexCount + (len(results)-15)
+        results = results[-15:]
+        countLimit += 100
+        while count < countLimit:
+            results += str(count)
+            count += 1
+            capLimit += 1
+
+#     print("this is the index count")
+#     print(indexCount)
+    results_prime = results.find(string) + indexCount
+#     print("this is results prime")
+#     print(results_prime)
+    return results_prime
+    
+            
+# while return results.find(string) results[-15:]
+# results[-15:] toate index si le adaug  la result
