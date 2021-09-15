f4df1d024238b883da6e0b1a751c0dff5ff66242
lucian <37814580+lucianmot@users.noreply.github.com>
2021-09-15 09:51:51 +0100

    Update Javascript - Is a number prime?.js

--- Javascript - Is a number prime?.js
+++ Javascript - Is a number prime?.js
@@ -2,15 +2,28 @@
 // https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript
 
 function isPrime(num) {
-//   if (num <= 1)
-//     return false;
-//   for (let i = 3; i < num/2; i + 2)
-//     if (num % i === 0) 
-//       return false;
-//   return true;
   
-  for  ( let i = 2; i < num; i ++)
-    if (num % i === 0) return false;
+  console.log('\n-----------------\n');
+  console.log('num: ', num);
+  
+  if(num < 1)
+    return false;
+  
+  if(num === 2 || num === 3)
+    return true;
+  
+  if( num%2 === 0 || num%3 === 0)
+    return false;
+  
+  for (let i = 1; 6*i+1 <= num/2; i++) {
+      console.log('\ni: ', i);
+      console.log(`6n-1: ${6*i-1}`);
+      console.log(`6n+1: ${6*i+1}`);
+    if ( num % (6*i-1) === 0 || num % (6*i+1) === 0 ){
+      return false;
+      }
+    }
+  
   return num > 1;
 }
 
