8cb62d4cdbabb4c89102c37f04586fddf5c26d77
lucian <37814580+lucianmot@users.noreply.github.com>
2021-11-09 08:55:35 +0000

    Create Javascript - Integers: Recreation One.js

---
+++ Javascript - Integers: Recreation One.js
@@ -0,0 +1,11 @@
+// Integers: Recreation One
+// https://www.codewars.com/kata/55aa075506463dac6600010d
+
+function listSquared(m, n) {
+  //   1. loop over numbers between m -> n (c)
+  //   2. get all divisors of c   2 -> x/2   x%i === 0;  sum += i*i
+  //   3. get sum of sqaured divisors of c
+  //   4. check if sum from 3. is a square in itself math.sqrt(sum)  math.pow(num, 2) = num * num
+  //   5. if sqare save in solution array
+  //   6. outside of the loop return solution array
+}
