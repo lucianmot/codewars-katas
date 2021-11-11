e3767a2fdb592bc1e2f55d7c2554986c234875ae
lucian <37814580+lucianmot@users.noreply.github.com>
2021-11-11 20:04:40 +0000

    Create Rust - Vowel Count.rs

---
+++ Rust - Vowel Count.rs
@@ -0,0 +1,15 @@
+// Rust - Vowel Count.rs
+// https://www.codewars.com/kata/54ff3102c1bad923760001f3
+
+//Incomplete
+
+fn get_count(string: &str) -> usize {
+    let mut vowels_count: usize = 0;
+    
+        println!(string);
+    
+    for i in 0..10 {
+    }
+    
+    vowels_count
+}
