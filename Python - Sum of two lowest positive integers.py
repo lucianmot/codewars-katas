69b12c93d59b7ae3e550cc6f7f841c1b58aa5281
lucian <37814580+lucianmot@users.noreply.github.com>
2021-11-30 13:29:00 +0000

    Create Python - Sum of two lowest positive integers.py

---
+++ Python - Sum of two lowest positive integers.py
@@ -0,0 +1,6 @@
+# Python - Sum of two lowest positive integers
+# https://www.codewars.com/kata/558fc85d8fd1938afb000014/train/python 
+
+def sum_two_smallest_numbers(numbers):
+    numbers.sort()
+    return numbers[0] + numbers[1]
