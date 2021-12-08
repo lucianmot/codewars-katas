e24788e574263eaaef203385ccda5911d043ad09
lucian <37814580+lucianmot@users.noreply.github.com>
2021-12-08 15:55:54 +0000

    Update Python - The position of a digital string in a infinite digital string.py

--- Python - The position of a digital string in a infinite digital string.py
+++ Python - The position of a digital string in a infinite digital string.py
@@ -1,4 +1,4 @@
-# Python - The position of a digital string in a infinite digital string
+# Python - The position of a digital string in a infinite digital string 
 # https://www.codewars.com/kata/582c1092306063791c000c00/train/python
 
 # attempt 1
