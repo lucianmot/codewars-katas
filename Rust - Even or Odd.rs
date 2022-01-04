5816e13501cad16f9609dd53087ce05c98c60a20
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-04 18:06:34 +0000

    Create Rust - Even or Odd.rs

---
+++ Rust - Even or Odd.rs
@@ -0,0 +1,6 @@
+// Rust - Even or Odd
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/rust
+
+fn even_or_odd(i: i32) -> &'static str {
+  if i % 2 == 0  { "Even" } else { "Odd" }
+}
