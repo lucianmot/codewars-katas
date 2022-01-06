7e3b074e90870c147068272a3ca5630efba4a716
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-06 19:51:16 +0000

    Update Rust - Counting sheep.rs

--- Rust - Counting sheep.rs
+++ Rust - Counting sheep.rs
@@ -1,14 +1,16 @@
 // Rust - Counting sheep...
 // https://www.codewars.com/kata/54edbc7200b811e956000556/train/rust
-// INCOMPLETE
+
+//  fn count_sheep(sheep: &[bool]) -> u8 {
+//    let mut results: u8 = 0;
+//    for i in sheep.iter() {
+//        if *i == true {
+//            results += 1;
+//        }
+//    }
+//    return results
+//  }
 
 fn count_sheep(sheep: &[bool]) -> u8 {
-//     let mut shit = sheep.retain(|&x| x == true);
-//     println!("{}", shit);
-//     return 1;
-    if let Some(pos) = sheep.iter().position(|x| *x != true) {
-        sheep.remove(pos);
-    }
-    println!("{:?}", sheep);
-    return 1
-}
+  sheep.iter().filter(|&&x|x).count() as u8
+} 
