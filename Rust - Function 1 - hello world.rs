c5708c17895ef0ce992a39b53d10e0506ae63f3f
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-07 07:46:09 +0000

    Create Rust - Function 1 - hello world.rs

---
+++ Rust - Function 1 - hello world.rs
@@ -0,0 +1,10 @@
+// Rust - Function 1 - hello world
+// https://www.codewars.com/kata/523b4ff7adca849afe000035/train/rust
+
+//  fn greet() -> String {
+//    "hello world!".to_string()
+//  }
+
+fn greet() -> &'static str {
+    "hello world!"
+}
