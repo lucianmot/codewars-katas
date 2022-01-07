adad11f44e67ea2c6e55ba268a63ce66b4205c43
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-07 08:50:35 +0000

    Create Rust - Is n divisible by x and y.rs

---
+++ Rust - Is n divisible by x and y.rs
@@ -0,0 +1,6 @@
+// Rust - Is n divisible by x and y?
+// https://www.codewars.com/kata/5545f109004975ea66000086/train/rust
+
+fn is_divisible(n: i32, x: i32, y: i32) -> bool {
+    n %  x == 0 && n % y == 0
+}
