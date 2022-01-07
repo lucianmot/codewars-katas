e528620b3c549e82e4d7a99e563c61c8e79f91de
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-07 08:55:32 +0000

    Create Rust - Returning Strings.rs

---
+++ Rust - Returning Strings.rs
@@ -0,0 +1,6 @@
+// Rust - Returning Strings
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/rust
+
+fn greet(name: &str) -> String {
+    format!("Hello, {} how are you doing today?", name)
+}
