785518560eaaf8c81d4e4a8706296d6d64827519
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-07 07:41:32 +0000

    Create Rust - Keep Hydrated.rs

---
+++ Rust - Keep Hydrated.rs
@@ -0,0 +1,10 @@
+// Rust - Keep Hydrated!
+// https://www.codewars.com/kata/582cb0224e56e068d800003c/train/rust
+
+//  fn litres(time: f64) -> i32 {
+//    (time / 2.0).floor() as i32
+//  }
+
+fn litres(time: f64) -> i32 {
+    time as i32 / 2
+}
