e9ba69fe59e0cb97960a45a8b2e71eb522d28412
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-10 22:16:15 +0000

    Create Rust - You Can't Code Under Pressure 1.rs

---
+++ Rust - You Can't Code Under Pressure 1.rs
@@ -0,0 +1,10 @@
+// Rust - You Can't Code Under Pressure #1
+// https://www.codewars.com/kata/53ee5429ba190077850011d4/train/rust
+
+//  fn double_integer(n: i32) -> i32 {
+//    n + n
+//  }
+
+fn double_integer(n: i32) -> i32 {
+    n * 2
+}
