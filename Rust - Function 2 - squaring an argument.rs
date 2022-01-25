70adbae8722cd477125083492a4ab1151417aaef
lucian <37814580+lucianmot@users.noreply.github.com>
2022-01-25 16:24:13 +0000

    Create Rust - Function 2 - squaring an argument.rs

---
+++ Rust - Function 2 - squaring an argument.rs
@@ -0,0 +1,10 @@
+// Rust - Function 2 - squaring an argument
+// https://www.codewars.com/kata/523b623152af8a30c6000027/train/rust
+
+//  fn square (n: i32) -> i32 {
+//    n.pow(2)
+//  }
+
+fn square(n: i32) -> i32 {
+    n * n
+}
