d81ee24e3f14d345b2d1856844a57b9be9b6328c
lucian <37814580+lucianmot@users.noreply.github.com>
2022-02-03 09:51:49 +0000

    Update Rust - Highest and Lowest.rs

--- Rust - Highest and Lowest.rs
+++ Rust - Highest and Lowest.rs
@@ -1,5 +1,5 @@
 // Rust - Highest and Lowest
-// https://www.codewars.com/kata/554b4ac871d6813a03000035/train/rust
+// https://www.codewars.com/kata/554b4ac871d6813a03000035/train/rust 
 
 fn high_and_low(numbers: &str) -> String {
     let numbers_array: Vec<i32> = numbers.split_whitespace().map(|x| x.parse().unwrap()).collect();
