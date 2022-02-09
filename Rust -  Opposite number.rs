4db280a3f78b15efa35eb39cc8f3d8bbe0577b6d
lucian <37814580+lucianmot@users.noreply.github.com>
2022-02-09 12:07:34 +0000

    Update Rust -  Opposite number.rs

--- Rust -  Opposite number.rs
+++ Rust -  Opposite number.rs
@@ -1,4 +1,4 @@
 // Rust -  Opposite number.rs
-// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/rust  
+// https://www.codewars.com/kata/56dec885c54a926dcd001095/train/rust
 
 fn opposite(n: i32) -> i32 {-n}
