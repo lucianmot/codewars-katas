3de9d6730df83b0b978820e056b9d7f0ba1fd015
lucian <37814580+lucianmot@users.noreply.github.com>
2022-02-10 09:30:18 +0000

    Update Javascript -  Dubstep.js

--- Javascript -  Dubstep.js
+++ Javascript -  Dubstep.js
@@ -1,5 +1,5 @@
 // Javascript - Dubstep
-// https://www.codewars.com/kata/551dc350bf4e526099000ae5/train/javascript 
+// https://www.codewars.com/kata/551dc350bf4e526099000ae5/train/javascript
 
 function songDecoder(song){  
   return song.replace(/WUB/g, ' ').replace(/\s\s+/g, ' ').trim();     
