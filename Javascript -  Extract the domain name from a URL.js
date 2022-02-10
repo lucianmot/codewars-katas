43bcd00055886ebb83838a63e7abcd97917a7cb1
lucian <37814580+lucianmot@users.noreply.github.com>
2022-02-10 09:29:41 +0000

    Update Javascript -  Extract the domain name from a URL.js

--- Javascript -  Extract the domain name from a URL.js
+++ Javascript -  Extract the domain name from a URL.js
@@ -1,5 +1,5 @@
 // Javascript -  Extract the domain name from a URL.js
-// https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
+// https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript 
 
 function domainName(url){
   return url.replace('http://', '').replace('https://', '').replace('www.', '').split('.')[0];
