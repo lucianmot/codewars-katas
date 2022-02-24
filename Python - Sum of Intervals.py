fd4f0645f92281055492ff654b7855204a6aa97b
lucian <37814580+lucianmot@users.noreply.github.com>
2022-02-24 11:47:47 +0000

    Update Python - Sum of Intervals.py

--- Python - Sum of Intervals.py
+++ Python - Sum of Intervals.py
@@ -1,5 +1,5 @@
 # Python - Sum of Intervals
-#https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/python  
+#https://www.codewars.com/kata/52b7ed099cdc285c300001cd/train/python 
 
 def sum_of_intervals(intervals):     
     result = [];
