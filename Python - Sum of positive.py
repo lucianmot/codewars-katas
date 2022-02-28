37bac6467cd71b27ffc43d251b065439c0a46fe1
lucian <37814580+lucianmot@users.noreply.github.com>
2022-02-28 17:55:49 +0000

    Update Python - Sum of positive.py

--- Python - Sum of positive.py
+++ Python - Sum of positive.py
@@ -1,5 +1,5 @@
 # Sum of positive
-# https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python.
+# https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
 
 def positive_sum(arr):
     results = 0
