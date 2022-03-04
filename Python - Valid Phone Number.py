bb098555671e8b48210a53f34c0f617d1f9a9e14
lucian <37814580+lucianmot@users.noreply.github.com>
2022-03-04 08:22:38 +0000

    Update Python - Valid Phone Number.py

--- Python - Valid Phone Number.py
+++ Python - Valid Phone Number.py
@@ -1,5 +1,5 @@
 # Python - Valid Phone Number 
-# https://www.codewars.com/kata/525f47c79f2f25a4db000025/train/python
+# https://www.codewars.com/kata/525f47c79f2f25a4db000025/train/python 
 
 import re
 def valid_phone_number(phone_number):
