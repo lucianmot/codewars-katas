5e813c998a89c68eeda34a448988a648fdac72bf
lucian <37814580+lucianmot@users.noreply.github.com>
2022-03-09 09:23:22 +0000

    Update C++ - Even or Odd.cpp

--- C++ - Even or Odd.cpp
+++ C++ - Even or Odd.cpp
@@ -1,9 +1,17 @@
 // C++ - Even or Odd
 // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/cpp
 
+/*
 #include <string>
 
 std::string even_or_odd(int number) {
   std::string result = (number % 2 == 0) ? "Even" : "Odd";
   return result;
+}
+*/
+
+#include <string>
+
+std::string even_or_odd(int number) {
+  return number % 2 == 0 ? "Even" : "Odd";
 }
