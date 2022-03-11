0e6d218963acb1721514fc0bd053fd3f8b687298
lucian <37814580+lucianmot@users.noreply.github.com>
2022-03-11 15:01:06 +0000

    Create C++ - Convert a Number to a String.cpp

---
+++ C++ - Convert a Number to a String.cpp
@@ -0,0 +1,8 @@
+// C++ - Convert a Number to a String!
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/cpp
+
+#include <string>
+
+std::string number_to_string(int num) {
+  return std::to_string(num);
+}
